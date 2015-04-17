6fbddbea1e0a08e7a8d8fcf79db2552a871278df
Filipe Molina <filipemolina@live.com>
2015-04-17 15:50:15 -0300

    Ajustes no Javascript do Select e do Botao

--- js/script.js
+++ js/script.js
@@ -2,11 +2,26 @@
 
 App = {};
 
-	App.nome = "";
-	App.email = "";
-	App.telefone = "";
-	App.modelo = "";
-	App.existe = false;
+// Funções globais
+
+function getDados(concessionaria)
+{
+	// As concessionárias nessa lista devem estar na mesma ordem em que aparecem no select do index
+
+	concessionarias = [
+		{ nome : "", telefone : "" },
+		{ nome : "Inter Japan - Botafogo", telefone : "(21) 21769400" },
+		{ nome : "Inter Japan - Barra", telefone : "(21) 8888-8888" },
+	];
+
+	return concessionarias[concessionaria];
+}
+
+function mostraDados(dados)
+{
+	$("span.local-span").html(dados.nome);
+	$("span.telefone-span").html(dados.telefone);
+}
 
 // Eventos principais
 
@@ -25,132 +40,22 @@ $(document).ready(function(){
 		//'slideEndAnimation': 	true
 	});
 
-	// Mostrar o nome do usuário correspondente ao número da carteirinha digitado
+	// Select de Telefones
 
-	$("input.carteirinha").blur(function(){
+	$("select.select-telefone").change(function(){
 
-		console.log("chamou a função");
+		var concessionaria = $(this).val();
 
-		var numero_carteirinha = $(this).val();
-
-		$.post('includes/busca.php', { carteirinha : numero_carteirinha }, function(data){
-
-			if(data != 'inexistente')
-			{
-				var dados = JSON.parse(data);
-
-				// Preencher os dados do formulário
-
-				// $("input[name=nome]").val(dados.nome);
-
-				//Preencher as variáveis com os dados do usuário
-
-				App.nome = dados.nome;
-				App.email = dados.email;
-				App.telefone = dados.telefone;
-				App.modelo = dados.modelo;
-				App.carteira = dados.carteira;
-				App.existe = true;
-			}
-
-		});
+		mostraDados(getDados(concessionaria));
 
 	});
 
-	// Realizar o cadastro via Ajax usando apenas o número da carteirinha e a quantidade de acompanhantes
+	// Botão para abrir o chat
 
-	$("form.cadastro_carteirinha").submit(function(event){
+	$("a.button").click(function(){
 
-		// Evitar que o formulário seja enviado de forma tradicional
-
-		event.preventDefault();
-
-		// Obter o número de acompanhnates
-
-		var acompanhantes = $("input[name='acompanhantes_carteirinha']").val();
-
-		/////////////////////////////////// Validação Básica
-
-		if(acompanhantes == "" || (!$.isNumeric(acompanhantes)))
-		{
-			$("div.alert").removeClass('alert-success').addClass('alert-danger').html("Preencha o campo 'Acompanhantes' corretamente.");
-			return false;
-		}
-
-		/////////////////////////////////// Fim da Validação Básica
-
-		// Testar se o número da carteirinha existe no banco de dados
-
-		if(App.existe)
-		{
-
-			// Enviar os dados via AJAX para o cadastro
-
-			$.post('includes/cadastro.php', { nome : App.nome, email : App.email, acompanhantes : acompanhantes, telefone : App.telefone, modelo : App.modelo, carteira : App.carteira }, function(data){
-
-				if(data == '')
-				{
-					$("div.alert").removeClass('alert-danger').addClass('alert-success').html(App.nome + ", seu cadastro foi realizado com sucesso!");
-
-					$("input[name='carteirinha']").val('');
-					$("input[name='acompanhantes_carteirinha']").val('');
-				}
-				else
-				{
-					$("div.alert").removeClass('alert-success').addClass('alert-danger').html(data);
-				}
-
-			});
-		}
-		else
-		{
-			$("div.alert").removeClass('alert-success').addClass('alert-danger').html("Preencha o campo 'Nº da Carteirinha' corretamente.");
-		}
-
-		return false;
+		$zopim.livechat.window.show();
 
 	});
-
-	// Enviar o formulário via Ajax
-
-	$("form.form-cadastro").submit(function(event){
-
-		event.preventDefault();
-
-		var nome = $("input[name='nome']").val();
-		var email = $("input[name='email']").val();
-		var acompanhantes = $("input[name='acompanhantes']").val();
-		var telefone = $("input[name='telefone']").val();
-		var modelo = $("input[name='modelo']").val();
-		var local_partida = $("select[name='local_partida']").val();
-
-		$.post('includes/cadastro.php', { nome : nome, email : email, acompanhantes : acompanhantes, telefone : telefone, modelo : modelo, local_partida : local_partida }, function(data){
-
-			console.log(data);
-
-			if(data == '')
-			{
-				$("div.alert").removeClass('alert-danger').addClass('alert-success').html("Cadastro realizado com sucesso!");
-
-				$("input[name='nome']").val('');
-				$("input[name='email']").val('');
-				$("input[name='acompanhantes']").val('');
-				$("input[name='telefone']").val('');
-				$("input[name='modelo']").val('');
-				$("select[name='local_partida']").val('');
-			}
-			else
-			{
-				$("div.alert").removeClass('alert-success').addClass('alert-danger').html(data);
-			}
-
-		});
-
-		return false;
-
-	});
-
-	// Máscara do campo
-
-	$("input[name='telefone']").mask('(99) 9999-9999?9');
+	
 });
